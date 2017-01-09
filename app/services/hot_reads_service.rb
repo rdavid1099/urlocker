e0ca586f33dcd733af1a172ce9317ad645bfb43f
Ryan Workman <rdavid1099@gmail.com>
2017-01-09 14:27:33 -0700

    Working on implementing service

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,3 +1,15 @@
 class HotReadsService
+  def self.record_read(link)
+    Read.create(link: link)
+    conn.post '/api/v1/reader', {url: link.url, title: link.title}
+  end
 
+  private
+    def self.conn
+      url = (ENV['RAILS_ENV'] == 'test' ? 'http://127.0.0.1:2000' : 'https://hot-reader.herokuapp.com')
+      Faraday.new(url: url) do |faraday|
+        faraday.request  :url_encoded
+        faraday.adapter  Faraday.default_adapter
+      end
+    end
 end
