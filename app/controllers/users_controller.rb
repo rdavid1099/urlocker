7c067258098acdeab0f1d9b7153be208462345a6
Ryan Workman <rdavid1099@gmail.com>
2017-01-09 09:43:48 -0700

    User can create a new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Account Successfully Created'
+      session[:user_id] = @user.id
       redirect_to :root
     else
-      redirect_to :signup
+      flash.now[:danger] = @user.errors.full_messages.join(', ')
+      render :new
     end
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
