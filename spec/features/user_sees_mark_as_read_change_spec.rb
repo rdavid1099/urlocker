fad2fc789698fd5caff9146862375e32e108f5c5
Ryan Workman <rdavid1099@gmail.com>
2017-01-09 15:57:17 -0700

    Add test for marking as read style changes

--- spec/features/user_sees_mark_as_read_change_spec.rb
+++ spec/features/user_sees_mark_as_read_change_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'reads link', :js => :true do
+describe 'marking as read changes style', :js => :true do
   let(:user) {create_user}
 
   before do
@@ -10,11 +10,18 @@ describe 'reads link', :js => :true do
   end
 
   context 'user clicks mark as read' do
-    it 'marks link as read' do
+    it 'changes the style of the link' do
       visit '/'
 
       within('.link_buttons') {click_on 'Mark as Read'}
-      expect(Read.count).to eq(1)
+
+      expect(page).to have_content('Mark as Unread')
+      expect(page).to have_content('Read? true')
+
+      within('.link_buttons') {click_on 'Mark as Unread'}
+
+      expect(page).to have_content('Mark as Read')
+      expect(page).to have_content('Read? false')
     end
   end
 end
