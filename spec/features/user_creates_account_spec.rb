88ffe9043c5cd68c078fe2d365b4661b32373f7a
Ryan Workman <rdavid1099@gmail.com>
2017-01-09 09:56:28 -0700

    Sign up fully tested and implemented
    Also removed aggrivating Chris Tucker gif

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -15,5 +15,58 @@ describe 'User logs in', :js => :true do
       expect(page).to have_content('Account Successfully Created')
       expect(page).to have_content('Welcome, test@test.com')
     end
+
+    it 'does not fill in email address and is rejected' do
+      visit '/'
+      click_on 'Sign Up'
+
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+
+      click_on 'Create Account'
+
+      expect(page).to have_content("Email can't be blank")
+      expect(current_path).to eq('/users')
+    end
+
+    it 'does not fill in password and is rejected' do
+      visit '/'
+      click_on 'Sign Up'
+
+      fill_in 'user_email', with: 'test@test.com'
+      fill_in 'user_password_confirmation', with: 'password'
+
+      click_on 'Create Account'
+
+      expect(page).to have_content("Password can't be blank")
+      expect(current_path).to eq('/users')
+    end
+
+    it 'does not fill in password confirm and is rejected' do
+      visit '/'
+      click_on 'Sign Up'
+
+      fill_in 'user_email', with: 'test@test.com'
+      fill_in 'user_password', with: 'password'
+
+      click_on 'Create Account'
+
+      expect(page).to have_content("Password confirmation can't be blank")
+      expect(current_path).to eq('/users')
+    end
+
+    it 'fills in different passwords and is rejected' do
+      visit '/'
+      click_on 'Sign Up'
+
+      fill_in 'user_email', with: 'test@test.com'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: '123456'
+
+      click_on 'Create Account'
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
+      expect(current_path).to eq('/users')
+    end
   end
 end
