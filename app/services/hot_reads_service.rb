527e92f0ac0af4b2e5db2d034af6ea3fdaa8d33a
Ryan Workman <rdavid1099@gmail.com>
2017-01-09 14:50:41 -0700

    Feature test for marking link as read

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -6,7 +6,7 @@ class HotReadsService
 
   private
     def self.conn
-      url = (ENV['RAILS_ENV'] == 'test' ? 'http://127.0.0.1:2000' : 'https://hot-reader.herokuapp.com')
+      url = (ENV['RAILS_ENV'] == 'production' ? 'https://hot-reader.herokuapp.com' : 'http://127.0.0.1:2000')
       Faraday.new(url: url) do |faraday|
         faraday.request  :url_encoded
         faraday.adapter  Faraday.default_adapter
---
+++ spec/features/user_marks_link_as_read_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'reads link', :js => :true do
+  let(:user) {create_user}
+
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    create_link(user)
+  end
+
+  context 'user clicks mark as read' do
+    it 'removes link from the list and database' do
+      VCR.use_cassette 'record read' do
+        visit '/'
+
+        within('.link_buttons') {click_link 'Mark as Read'}
+        expect(Read.count).to eq(1)
+      end
+    end
+  end
+end
