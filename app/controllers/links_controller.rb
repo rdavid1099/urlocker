463d4d19288618b14ab29e46c976c8507853a034
Ryan Workman <rdavid1099@gmail.com>
2017-01-09 12:17:43 -0700

    Sad path tested for updating link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,7 +2,6 @@ class LinksController < ApplicationController
   def index
     @hot_links = Link.hot
     @user_links = current_user.links if logged_in?
-    byebug
   end
 
   def edit
--- spec/features/user_edits_existing_link_spec.rb
+++ spec/features/user_edits_existing_link_spec.rb
@@ -3,24 +3,33 @@ require 'rails_helper'
 describe 'edit links', :js => :true do
   let(:user) {create_user}
 
-  context 'Authenticated user can edit an existing link' do
-    it 'enters valid URL for link and creates it' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      orig_link = create_link(user)
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    create_link(user)
+  end
 
+  context 'Authenticated user can edit an existing link' do
+    it 'enters valid URL and title for link and updates it' do
       visit '/'
       within('.link_buttons') {click_link 'Edit'}
       fill_in 'link_url', with: 'http://new.com'
       fill_in 'link_title', with: 'NEW'
       click_on 'Update Link'
 
-      within('#links-list') do
-        expect(page).to have_content('NEW')
-        expect(page).to have_content('http://new.com')
-        expect(page).to have_content('Link Successfully Updated')
-        expect(page).to_not have_content(orig_link.url)
-        expect(page).to_not have_content(orig_link.title)
-      end
+      expect(page).to have_content('Link Successfully Updated')
+      expect(Link.last.url).to eq('http://new.com')
+      expect(Link.last.title).to eq('NEW')
+    end
+
+    it 'enters invalid URL for link and is rejected' do
+      visit '/'
+      within('.link_buttons') {click_link 'Edit'}
+      fill_in 'link_url', with: 'new.com'
+      click_on 'Update Link'
+
+      expect(page).to have_content('Invalid URL. Please enter a valid web address.')
+      expect(Link.last.url).to eq('http://test.com')
+      expect(Link.last.title).to eq('test')
     end
   end
 end
